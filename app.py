fa1ecc67049e01033bffd07de72875d0eb942126
Gloria Macia <gloria.macia@roche.com>
2024-05-05 16:24:45 +0200

    modify port and footer

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def main():
     # Left column: Title and text
     with col1:
         st.title("Ask ChatNICE")
-        text = """An artificial intelligence chatbot trained on guidances from NICE. It is based on large language models like gpt-4. 
-        ChatNICE is a proof of concept for research purposes only. 
-        The user assumes full liability for any other use. This preview is trained on the 'Caesarean birth' guidance (NG192).
-        Contact g.macia-munoz@lse.ac.uk for further inquiries"""
+        text = """An artificial intelligence chatbot trained on guidances from NICE. It is based on a large language model like gpt-4. 
+        ChatNICE is a proof of concept for research purposes only. This webite is not affiliated with nice.org.uk
+        The user assumes full liability for any other use. This preview is trained on the 'Caesarean birth' guidance (NG192). 
+        Contact Gloria Macia at g.macia-munoz@lse.ac.uk for further inquiries"""
         # You can add more text or other elements as needed.
         st.markdown(text)
 
@@ -25,30 +25,16 @@ def main():
         image_path = os.path.join('assets', 'chat.png')
         st.image(image_path, use_column_width=True)
     
-        # Embed the chat bubble code
+    # Embed the chat bubble code
     st.components.v1.iframe("https://www.chatbase.co/chatbot-iframe/DcpTIYic0VkmgY2JyNd4Z", height=500)
-    # Dark footer with contact details
-    st.markdown(
-        """
-        <style>
-            .footer {
-                position: fixed;
-                bottom: 0;
-                left: 0;
-                width: 100%;
-                background-color: #333;
-                color: white;
-                padding: 5px;
-                text-align: center;
-                font-size: 10px;
-            }
-        </style>
-        <div class="footer">
-            <p> © 2023 by Gloria Macia. This webite is not affiliated with nice.org.uk</p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
